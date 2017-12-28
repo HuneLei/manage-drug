d92e755c49104ae79235ce0ae11140f893f1f0cc
HuneLei <wodota0909@163.com>
2017-12-28 10:53:37 +0800

    change

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello Hune',
+    motto: 'Hello HuneLei',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
